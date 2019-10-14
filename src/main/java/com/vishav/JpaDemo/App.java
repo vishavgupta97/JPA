0e36d17f47a1b2d926cb3e89ea90604afb38844c
vishav <vishavgupta988@gmail.com>
2019-10-14 15:58:40 +0530

    INITIAL COMMIT

---
+++ src/main/java/com/vishav/JpaDemo/App.java
@@ -0,0 +1,20 @@
+package com.vishav.JpaDemo;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	EntityManagerFactory emf = Persistence.createEntityManagerFactory("");
+    	EntityManager em = emf.createEntityManager();
+     //   Alien a = em.find(Alien.class,4);
+    }
+}
